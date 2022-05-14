8031623b736a3b9d8765f6dae2b998af7e8c71f7
George Mihov <gmihov001@gmail.com>
2022-05-14 16:39:55 +0000

    Navbar conditional rendering

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import PropTypes from "prop-types";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
+
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
         <Link to="/">
           <span className="navbar-brand mb-0 h1">PayMeApp</span>
         </Link>
-        <div className="ml-auto">
-          <Link to="/demo">
-            <button className="btn btn-primary">
-              Check the Context in action
-            </button>
-          </Link>
-        </div>
+        {store.user ? (
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Dashboard</button>
+            </Link>
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Marketplace</button>
+            </Link>
+          </div>
+        ) : (
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Log In</button>
+            </Link>
+            <Link to="/demo">
+              <button className="btn btn-primary mx-2">Sign Up</button>
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  match: PropTypes.object,
 };
