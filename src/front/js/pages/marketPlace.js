20c50a0b0e474763c084b8356470fb79182aaa9d
George Mihov <gmihov001@gmail.com>
2022-05-14 16:08:29 +0000

    test

--- src/front/js/pages/marketPlace.js
+++ src/front/js/pages/marketPlace.js
@@ -3,15 +3,18 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const MarketPlace = props => {
-    const { store, actions } = useContext(Context);
-    const params = useParams();
+export const MarketPlace = (props) => {
+  const { store, actions } = useContext(Context);
+  const params = useParams();
 
-    return (
-        <div className="container-fluid">MarketPlace</div>
-    );
+  return (
+    <div className="container-fluid">
+      MarketPlace
+      <span>hello</span>
+    </div>
+  );
 };
 
 MarketPlace.propTypes = {
-    match: PropTypes.object
+  match: PropTypes.object,
 };
