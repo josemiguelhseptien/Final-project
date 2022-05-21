cd461b4865b4422e9065b99f9a03a12adace2c65
jesiallen <justjesi2010@gmail.com>
2022-05-21 16:36:24 +0000

    updated React & built dashboard money view

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       message: null,
+      calendarEntries: [],
       demo: [],
       user: "username",
     },
