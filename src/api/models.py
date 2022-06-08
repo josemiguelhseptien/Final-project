4c03ab7a55b556972f6a33dfc656e5a6e6da1fe6
jesiallen <justjesi2010@gmail.com>
2022-06-08 19:07:03 +0000

    added database & api routes for backend

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,114 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+# ---------------------User Types--------------------
+
+class AccountUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    profile_picture = db.Column(db.String(200), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    background = db.Column(db.String(80), unique=False, nullable=False)
+    user_type = db.Column(db.String(80), unique=False, nullable=False)
+    about = (db.Text(), nullable=False)
+    services = db.Column(db.String(80), unique=False, nullable=False)
+    prices = db.Column(db.Integer, nullable=False)
+    zip_code = db.Column(db.Integer, nullable=False)
+    class_format = db.Column(db.String(80), unique=False, nullable=False)
+    availability_monday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_tuesday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_wednesday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_thursday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_friday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_saturday = db.Column(db.String(80), unique=False, nullable=False)
+    availability_sunday = db.Column(db.String(80), unique=False, nullable=False)
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<AccountUser %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            "profilePicture" : self.profile_picture,
+            "name" : self.name,
+            "phone" : self.phone,
+            "background" : self.background,
+            "userType" : self.user_type,
+            "about" : self.about,
+            "services" : self.services,
+            "prices" : self.prices,
+            "zip_code" : self.zip_code,
+            "format" : self.class_format,
+            "availability.monday": self.availability_monday, 
+            "availability.tuesday" : self.availability_tuesday,
+            "availability.wednesday" : self.availability_wednesday,
+            "availability.thursday" : self.availability_thursday,
+            "availability.friday" : self.availability_friday,
+            "availability.saturday" : self.availability_saturday,
+            "availability.sunday" : self.availability_sunday,
+        }
+
+class ClientUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    userType = db.Column(db.String(80), unique=False, nullable=False)
+    zip_code = db.Column(db.Integer, nullable=False)
+
+
+    def __repr__(self):
+        return '<ClientUser %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+            "name" : self.name,
+            "phone" : self.phone,
+            "userType" : self.userType,
+            "zip_code" : self.zip_code,
+            "format" : self.class_format,
+        }
+
+
+
+# --------------------- Other Info to Store--------------------
+
+class CalendarEntries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider_id=db.Column(db.Integer, db.ForeignKey('AccountUser.id'))
+    client_id=db.Column(db.Integer, db.ForeignKey('ClientUser.id'))
+    client_name = db.Column(db.String(120), unique=False, nullable=False)
+    start_date = db.Column(db.String(120), unique=False, nullable=False)
+    end_date = db.Column(db.String(120), unique=False, nullable=False)
+    all_day = db.Column(db.Boolean(), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    recurrence_rule = db.Column(db.String(120), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<CalendarEntries %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "provider_id" : self.provider_id,
+            "client_id" : self.client_id,
+            "text" : self.client_name,
+            "startDate" : self.start_date,
+            "endDate" : self.end_date,
+            "allDay" : self.all_day,
+            "description" : self.description,
+            "recurrenceRule" : self.recurrence_rule,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,76 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# -------------Pro User Info------------------
+
+@api.route('/pro/<int:id>', methods=['GET'])
+def get_account_user(acct_id):
+    account_user = AccountUser.query.filter_by(id=acct_id).first()
+    user_info = list(map(lambda x: x.serialize(), account_user))
+
+    return jsonify(user_info), 200
+
+@api.route('/pro', methods=['POST'])
+def post_account_user():
+    body=request.get_json()
+    user_info= AccountUser(email=body['email'], password=body['password'], profile_picture=body['profilePicture'], name=body['name'], phone=body['phone'], background=body['background'], user_type=body['userType'], about=body['about'], services=body['services'], prices=body['prices'], zip_code=body['zip_code'], class_format=body['format'], availability_monday=body['availability.monday'], availability_tuesday=body['availability.tuesday'], availability_wednesday=body['availability.wednesday'], availability_thursday=body['availability.thursday'], availability_friday=body['availability.friday'], availability_saturday=body['availability.saturday'], availability_sunday=body['availability.sunday'])
+
+    db.session.add(user_info)
+    db.session.commit()
+
+    return jsonify("Successfully added professional user"), 200
+
+# -------------Client User Info------------------
+
+@api.route('/client/<int:id>', methods=['GET'])
+def get_client_user(acct_id):
+    client_user = ClientUser.query.filter_by(id=acct_id).first()
+    user_info = list(map(lambda x: x.serialize(), client_user))
+
+    return jsonify(user_info), 200
+
+@api.route('/client', methods=['POST'])
+def post_account_user():
+    body=request.get_json()
+    user_info= ClientUser(email=body['email'], password=body['password'], name=body['name'], phone=body['phone'], user_type=body['userType'], zip_code=body['zip_code'])
+
+    db.session.add(user_info)
+    db.session.commit()
+
+    return jsonify("Successfully added client user"), 200
+
+# -------------Calendar Info------------------
+
+@api.route('/pro_appts/<int:id>', methods=['GET'])
+def get_appts(acct_id):
+    appts = CalendarEntries.query.filter_by(provider_id=acct_id).first()
+    appt_list = list(map(lambda x: x.serialize(), appts))
+
+    return jsonify(appt_list), 200
+
+@api.route('/pro_appts/<int:id>/', methods=['POST'])
+def post_appt(acct_id):
+    body=request.get_json()
+    appt_info= CalendarEntries(provider_id=body['provider_id'], client_id=body['client_id'], client_name=body['text'], start_date=body['startDate'], end_date=body['endDate'], all_day=body['allDay'], description=body['description'], recurrence_rule=body['recurrenceRule'])
+
+    db.session.add(appt_info)
+    db.session.commit()
+
+    appts = CalendarEntries.query.filter_by(provider_id=acct_id).first()
+    appt_list = list(map(lambda x: x.serialize(), appts))
+
+    return jsonify(appt_list), 200
+
+@api.route('/pro_appts/<int:id>/<int:id>/', methods=['DELETE'])
+def delete_appt(acct_id, appt_id):
+    appts = CalendarEntries.query.filter_by(id=appt_id).first()
+
+    db.session.delete(appts)
+    db.session.commit()
+
+    appts = CalendarEntries.query.filter_by(provider_id=acct_id).first()
+    appt_list = list(map(lambda x: x.serialize(), appts))
+
+    return jsonify(appt_list), 200
