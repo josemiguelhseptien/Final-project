6006cdc71591591a40d142738071dc356d940afb
jesiallen <justjesi2010@gmail.com>
2022-06-09 22:39:35 +0000

    adjusted clientside provider view layout

--- src/front/js/component/viewProviders.js
+++ src/front/js/component/viewProviders.js
@@ -14,23 +14,23 @@ export const ViewProviders = () => {
         <div className="container-fluid">
             <br></br>
             <div>
-                <h3>Your Providers</h3>
+                <h3 className="my-5">Your Providers</h3>
                 <div className="cardRow mb-5">
-                    <div className="card">
-                        <div style={{ width: "18rem" }}>
+                    <div className="card m-3">
+                        <div style={{ width: "18rem", height: "21rem" }}>
                             <div className="card-body">
                                 <img src={store.accountUser[0].profilePicture} className="card-img-top" />
-                                <h5 className="card-title">{store.accountUser[0].name}</h5>
+                                <h5 className="card-title my-4">{store.accountUser[0].name}</h5>
                                 <p className="card-text">{store.accountUser[0].phone} </p>
                                 <p className="card-text">{store.accountUser[0].email} </p>
                             </div>
                         </div>
                     </div>
-                    <div className="card">
-                        <div style={{ width: "18rem" }}>
+                    <div className="card m-3">
+                        <div style={{ width: "18rem", height: "21rem" }}>
                             <div className="card-body">
                                 <img src={store.accountUser[1].profilePicture} className="card-img-top" />
-                                <h5 className="card-title">{store.accountUser[1].name}</h5>
+                                <h5 className="card-title my-4">{store.accountUser[1].name}</h5>
                                 <p className="card-text">{store.accountUser[1].phone} </p>
                                 <p className="card-text">{store.accountUser[1].email} </p>
                             </div>
